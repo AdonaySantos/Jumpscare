95d96b00967e846b64f457f06e7616e9c4e27d86
Adonay <adonay.rssantos@gmail.com>
2025-04-19 03:04:02 -0300

    Setting up the configuration to build

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jumpscare",
+    version="0.0.1",
+    author="Adonay Santos",
+    description="A funny tool that scares developers when errors are found",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "watchdog",
+        "typer[all]"
+    ],
+    entry_points={
+        "console_scripts": [
+            "jumpscare = jumpscare.cli:app",
+        ],
+    },
+    package_data={
+        "jumpscare": ["releases/*.jpg", "releases/*.mp3"],
+    },
+    python_requires=">=3.9",
+)
